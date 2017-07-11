2b2f70a2f27512658557810c0223fb39fae6dbf1
Bravilogy <Bravilogy@users.noreply.github.com>
2017-07-11 12:55:31 +0100

    remove an unused test reducer action

--- src/js/reducer.js
+++ src/js/reducer.js
@@ -1,16 +1,9 @@
 import { merge } from 'ramda';
 
 const initialState = {
-    message: '',
     examples: []
 };
 
 export default function (state = initialState, action) {
-    switch(action.type) {
-        case 'DO_SOMETHING':
-            return merge(state, {
-                message: 'hello'
-            });
-    }
     return state;
 }
